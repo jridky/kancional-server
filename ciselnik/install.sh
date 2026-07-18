08c3c0ab99d314023999832cfb14801b5956623a
Josef Ridky <jridky@redhat.com>
2026-07-18 14:07:49 +0200

    Podpora HDMI-CEC pro číselníky a zprovoznení na bookworm a novějších
    Signed-off-by: Josef Ridky <jridky@redhat.com>

--- ciselnik/install.sh
+++ ciselnik/install.sh
@@ -5,8 +5,36 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+# Detekce verze OS
+. /etc/os-release
+IS_BOOKWORM_PLUS=false
+case "$VERSION_CODENAME" in
+    jessie|stretch|buster|bullseye)
+        IS_BOOKWORM_PLUS=false
+        ;;
+    *)
+        IS_BOOKWORM_PLUS=true
+        ;;
+esac
+
+# Dotaz na typ zobrazovacího zařízení
+USE_CEC=false
+read -p "Jaké zobrazovací zařízení bude připojeno - televize nebo monitor? (t/m): " CEC_ANSWER
+if [ "$CEC_ANSWER" = "t" ] ; then
+    USE_CEC=true
+fi
+
 #instalace balicku
-apt-get install apache2 php -y || { echo "Systému se nepodařilo nainstalovat potřebné balíky. V instalaci číselníku nelze pokračovat. Tip: Zkontrolujte si připojení k internetu."; exit 1; }
+PACKAGES="apache2 php"
+if [ "$USE_CEC" = true ] ; then
+    PACKAGES="$PACKAGES v4l-utils"
+fi
+if [ "$IS_BOOKWORM_PLUS" = true ] ; then
+    PACKAGES="$PACKAGES wlopm wtype x11-xserver-utils"
+elif [ "$USE_CEC" = true ] ; then
+    PACKAGES="$PACKAGES x11-xserver-utils"
+fi
+apt-get install $PACKAGES -y || { echo "Systému se nepodařilo nainstalovat potřebné balíky. V instalaci číselníku nelze pokračovat. Tip: Zkontrolujte si připojení k internetu."; exit 1; }
 
 #kopirovani nastaveni
 cp ./etc/apache2/ports.conf /etc/apache2/
@@ -31,22 +59,61 @@ cp ./var/www/html/* /var/www/html/
 chmod 0644 /var/www/html/*
 chown root:root /var/www/html/*
 
-# nastavení běhu na x11
-sed /etc/lightdm/lightdm.conf -i -e "s/^#\\?user-session.*/user-session=LXDE-pi-x/"
-sed /etc/lightdm/lightdm.conf -i -e "s/^#\\?autologin-session.*/autologin-session=LXDE-pi-x/"
-sed /etc/lightdm/lightdm.conf -i -e "s/^#\\?greeter-session.*/greeter-session=pi-greeter/"
-
-echo "Přepnuto na prostředí x11"
+if [ "$IS_BOOKWORM_PLUS" = false ] ; then
+    # nastavení běhu na x11
+    sed /etc/lightdm/lightdm.conf -i -e "s/^#\\?user-session.*/user-session=LXDE-pi-x/"
+    sed /etc/lightdm/lightdm.conf -i -e "s/^#\\?autologin-session.*/autologin-session=LXDE-pi-x/"
+    sed /etc/lightdm/lightdm.conf -i -e "s/^#\\?greeter-session.*/greeter-session=pi-greeter/"
+    echo "Přepnuto na prostředí x11"
+fi
 
 # Nastavení napájení
 sed -i 's/#xserver-command=X/xserver-command=X -s 0 dpms/g' /etc/lightdm/lightdm.conf
-if [ -e /boot/firmware/config.txt ] ; then
-    sed -i 's/^dtoverlay/#dtoverlay/g' /boot/firmware/config.txt
-else
-    sed -i 's/^dtoverlay/#dtoverlay/g' /boot/config.txt
+if [ "$IS_BOOKWORM_PLUS" = false ] && [ "$USE_CEC" = false ] ; then
+    if [ -e /boot/firmware/config.txt ] ; then
+        sed -i 's/^dtoverlay/#dtoverlay/g' /boot/firmware/config.txt
+    else
+        sed -i 's/^dtoverlay/#dtoverlay/g' /boot/config.txt
+    fi
+fi
+
+# Prevence hotplug waggle na Bookworm+
+if [ "$IS_BOOKWORM_PLUS" = true ] ; then
+    if [ -e /boot/firmware/cmdline.txt ] ; then
+        CMDLINE_FILE="/boot/firmware/cmdline.txt"
+    else
+        CMDLINE_FILE="/boot/cmdline.txt"
+    fi
+    if ! grep -q 'vc4.force_hotplug' "$CMDLINE_FILE" ; then
+        sed -i 's/$/ vc4.force_hotplug=3/' "$CMDLINE_FILE"
+    fi
 fi
 
 sudo usermod -aG video www-data
+
+# Na Bookworm+ povolit www-data spouštět wlopm jako pi (pro ovládání displeje z PHP)
+if [ "$IS_BOOKWORM_PLUS" = true ] ; then
+    echo "www-data ALL=(pi) NOPASSWD: /usr/bin/wlopm, /usr/bin/env" > /etc/sudoers.d/donoff
+    chmod 0440 /etc/sudoers.d/donoff
+fi
+
+# Na Bookworm+ nastavit skrytí kurzoru přes labwc
+if [ "$IS_BOOKWORM_PLUS" = true ] ; then
+    LABWC_DIR="/home/pi/.config/labwc"
+    mkdir -p "$LABWC_DIR"
+
+    # Zkopírovat systémový rc.xml pokud lokální neexistuje
+    if [ ! -e "$LABWC_DIR/rc.xml" ] ; then
+        cp /etc/xdg/labwc/rc.xml "$LABWC_DIR/rc.xml"
+    fi
+
+    # Přidat keybind pro HideCursor pokud ještě není
+    if ! grep -q 'HideCursor' "$LABWC_DIR/rc.xml" ; then
+        sed -i '/<\/keyboard>/i\    <keybind key="A-W-h">\n      <action name="HideCursor" \/>\n    <\/keybind>' "$LABWC_DIR/rc.xml"
+    fi
+
+    chown -R pi:pi "$LABWC_DIR"
+fi
 
 sudo systemctl enable startup
 
